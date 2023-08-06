6d4559cd1d6c40ac5cc346aae635aae9944692c9
AnnaK <njutcka@yandex.ru>
2023-08-06 16:38:01 +0700

    задание 3. Тесты работают

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,8 +5,12 @@ from utils import arrs
 class TestArrs(unittest.TestCase):
 
     def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 3)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
+        self.assertEqual(arrs.get([1, 2, 3], 1, 2), 2)
+
+    def test_get_index_error(self):
+        with self.assertRaises(IndexError):
+            arrs.get([], 1)
+
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
